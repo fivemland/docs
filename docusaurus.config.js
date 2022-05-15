8ba4e095f41a0841d545b8df458eee40c120f4dc
ADAM-ASZTALI\Adam <sepaadam66@gmail.com>
2022-05-15 02:14:01 +0200

    fivemland

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "Fivemland",
   tagline: "Egy  magyar FiveM fejlesztői közösség, csatlakozz hozzánk még ma!",
-  url: "https://your-docusaurus-test-site.com",
+  url: "https://fivemland.github.io/fivemlandocs/",
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
